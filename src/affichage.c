ce537f37112278e0a2b73ef76e880e03dc9c8155
R-Bas <robin.bastide@outlook.fr>
2019-02-22 15:06:04 +0100

    Ajout des numéros de lignes et colonnes dans afficher_plateau (à améliorer dans le futur)

--- src/affichage.c
+++ src/affichage.c
@@ -8,6 +8,7 @@
 void afficher_plateau(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU]) {
     
     for (int i = 0; i < TAILLE_PLATEAU; i++) {
+        printf("%3d ", TAILLE_PLATEAU - i);
         for (int j = 0; j < TAILLE_PLATEAU; j++) {
             switch(pl[i][j]) {
              
@@ -38,6 +39,13 @@ void afficher_plateau(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU]) {
         }
         printf("\n");
     }
+
+
+    printf("    ");
+    for (int i = 1; i <= TAILLE_PLATEAU; i++) {
+        printf("%2d", i);
+    }
+    printf("\n");
 }
 
 void afficher_pieces_dispo(Joueur* j) {
