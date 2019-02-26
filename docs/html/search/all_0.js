9a5cd296b8954c3dafd768c854155b42b77b722b
Bastide Robin <s172177@ic2s124-16.univ-lemans.fr>
2019-02-26 14:43:50 +0100

    Ajout de la doc affichage.c v2

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -1,4 +1,8 @@
 var searchData=
 [
-  ['blokus_5fin_5fc',['Blokus_in_c',['../md_README.html',1,'']]]
+  ['affichage_2ec',['affichage.c',['../affichage_8c.html',1,'']]],
+  ['afficher_5fchoix_5forientation',['afficher_choix_orientation',['../affichage_8c.html#a774f342c4bb2576f6b7e7cff62f577b3',1,'affichage.c']]],
+  ['afficher_5fpieces_5fdispo',['afficher_pieces_dispo',['../affichage_8c.html#ade7506941effd14df4d79d3dcd414493',1,'affichage.c']]],
+  ['afficher_5fplateau',['afficher_plateau',['../affichage_8c.html#af72ba27a8b6dd0a005e4462eee9ddac9',1,'affichage.c']]],
+  ['afficher_5fscores',['afficher_scores',['../affichage_8c.html#a0607bdb91a55dfb7df2eb1c68ad45040',1,'affichage.c']]]
 ];
--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,18 +1,27 @@
 var indexSectionsWithContent =
 {
-  0: "b",
-  1: "b"
+  0: "abcjp",
+  1: "cjp",
+  2: "a",
+  3: "a",
+  4: "b"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "pages"
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "Tout",
-  1: "Pages"
+  1: "Structures de données",
+  2: "Fichiers",
+  3: "Fonctions",
+  4: "Pages"
 };
 
