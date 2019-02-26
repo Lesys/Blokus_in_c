b813a18e550a1272302402e60ae6cef6f4124a7b
Bastide Robin <s172177@ic2s124-16.univ-lemans.fr>
2019-02-26 15:55:38 +0100

    MAJ doc

--- docs/html/search/files_0.js
+++ docs/html/search/files_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['affichage_2ec',['affichage.c',['../affichage_8c.html',1,'']]]
+  ['affichage_2ec',['affichage.c',['../affichage_8c.html',1,'']]],
+  ['affichage_2eh',['affichage.h',['../affichage_8h.html',1,'']]]
 ];
