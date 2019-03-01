35b3137bf0bea1216805555027e8ac237a6aa1e1
Riguidel Hugo <s171369@ic2s114-18.univ-lemans.fr>
2019-03-01 10:44:00 +0100

    Finalisation de la fonction demander_orientations

--- src/gestion_tour.c
+++ src/gestion_tour.c
@@ -9,9 +9,12 @@ Piece* demander_piece(Joueur* j)
 {
   int nb, i;
   Piece* p = joueur_liste_piece(j);
+
   printf("Quelle pièce voulez-vous jouer ?\n");
   afficher_pieces_dispo(j);
   scanf("%d", &nb);
+
+  /* tant que le joueur entre un numéro ne correspondant pas à une pièce disponible, on redemande un numéro valide */
   while((nb < 0) && (nb > joueur_nb_piece_restantes(j)))
   {
     printf("Veuillez entrer une valeur correcte\n");
@@ -19,14 +22,19 @@ Piece* demander_piece(Joueur* j)
     afficher_pieces_dispo(j);
     scanf("%d", &nb);
   }
+
+  /* on sélectionne la bonne pièce */
   for(i = 1; i < nb; i++)
     p = piece_suivant(p);
+
   return p;
 }
 
+/* Fonction initialisant une matrice de taille 5*5 en la remplissant de 0 */
 void initialiser_matrice(int matrice[5][5])
 {
   int i, j;
+
   for(i = 0; i < 5; i++)
   {
     for(j = 0; j < 5; j++)
@@ -34,30 +42,56 @@ void initialiser_matrice(int matrice[5][5])
   }
 }
 
+/* Fonction affectant une pièce à une matrice
+   remplit la matrice de 1 en fonction des coordonnées des carres constituant la pièce */
 void affecter_matrice(int matrice[5][5], Carre* c)
 {
   Carre* c2 = c;
   c2 = carre_get_suiv(c2);
+
   while(c2 != c)
   {
     matrice[carre_get_x(c2)][carre_get_y(c2)] = 1;
     c2 = carre_get_suiv(c2);
   }
+
+  matrice[carre_get_x(c2)][carre_get_y(c2)] = 1;
 }
+
+/* Fonction permettant d'afficher une matrice 5*5
+
+void afficher_matrice(int matrice[5][5])
+{
+  int i, j;
+
+  for(i = 4; i >= 0; i--)
+  {
+    for(j = 0; j < 5; j++)
+      printf("%d", matrice[i][j]);
+
+    printf("\n");
+  }
+
+  printf("\n");
+}
+*/
 
 /* Affiche les 4 orientations possibles de la pièce au joueur avec un numéro et attend qu'il entre un numéro */
 /* modifie également les coordonnees relatives des carres constituant la piece une fois l'orientation choisie */
 void demander_orientation(Piece* p)
 {
   int nb;
-  int max_x = 0;
-  int max_y = 0;
+  int min_x;
+  int min_y;
   Orientation o;
   Carre* c = piece_liste_carre(p);
+
   afficher_choix_orientation(p);
   printf("Dans quelle orientation voulez-vous jouer la pièce ? :\n");
   scanf("%d", &nb);
   nb--;
+
+  /* tant que le joueur n'entre pas un numéro d'orientation correct, on lui redemande un numéro valide */
   while((nb < HAUT) || (nb > GAUCHE))
   {
     printf("Veuillez entrer une orientation correcte\n");
@@ -66,7 +100,15 @@ void demander_orientation(Piece* p)
     scanf("%d", &nb);
     nb--;
   }
-  int sauv, i, j, k, l, m;
+
+  int i, j;
+
+  /* Pour chaque orientation, on tourne la pièce de 90° un certain nombre de fois
+     Orientation 1 : 0°
+     Orientation 2 : 90°
+     Orientation 3 : 180°
+     Orientation 4 : 270°
+  */
   for(nb; nb > 0; nb--)
   {
     int initiale[5][5];
@@ -74,60 +116,51 @@ void demander_orientation(Piece* p)
     affecter_matrice(initiale, c);
     int finale[5][5];
     initialiser_matrice(finale);
+
+    /* Modification des coordonnées des carres de la pièce dans une matrice temporaire */
     for(i = 0; i < 5; i++)
     {
-      k = l = m = 0;
       for(j = 0; j < 5; j++)
       {
         if (initiale[i][j])
         {
-          l = i;
-          m = j;
-          sauv = m;
-          m = 5 - k;
-          l = sauv;
-          finale[l][m] = 1;
-          k++;
+          finale[4 - j][i] = 1;
         }
       }
     }
+
+    min_x = 5;
+    min_y = 5;
+
+    /* On calcul les coordonnees minimales des carres constituant la pièce */
     for(i = 0; i < 5; i++)
     {
       for(j = 0; j < 5; j++)
       {
         if(finale[i][j])
         {
-            c->x = i;
-            c->y = j;
-            c = carre_get_suiv(c);
+          if(i < min_x)
+            min_x = i;
+
+          if(j < min_y)
+            min_y = j;
+
+          c->x = i;
+          c->y = j;
+          c = carre_get_suiv(c);
         }
       }
     }
-  }
-  /*switch(nb)
-  {
-    case HAUT:
-      break;
-    case DROITE:
-      sauv = c->x; c->x = c->y; c->y = -(c->x); break;
-    case BAS:
-      sauv = c->x; c->x = -(c->y); c->y = -(c->x); break;
-    case GAUCHE:
-      sauv = c->x; c->x = c->y; c->y = c->x; break;
-  }
-  if(max_x > c->x)
-    max_x = c->x;
-  if(max_y > c->y)
-    max_y = c->y;
-  c = carre_get_suiv(c);*/
 
-  /*c = carre_get_suiv(c);
-  while(c != piece_liste_carre(p))
-  {
-    c->x += max_x;
-    c->y += max_y;
-    c = carre_get_suiv(c);
-  }*/
+    c = piece_liste_carre(p);
+    Carre* c2 = c;
+
+    /* On réaffecte les coordonnees minimales des carres constituant la pièce de sorte que x et y >= 0 (On ramène la pièce dans le coin inférieur gauche) */
+    do {
+      c->x -= min_x;
+      c->y -= min_y;
+    } while ((c = carre_get_suiv(c)) != c2);
+  }
 }
 
 int verification_position(Couleur pl[20][20], int x, int y)
