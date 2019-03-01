16d3fd55d0d9e325731ecc6c80385b99c43d7a8b
Snalts <alexandre.jodeau@orange.fr>
2019-03-01 11:29:06 +0100

    Maj doc le 01/03/19 à 11:29:04

--- docs/html/search/all_4.js
+++ docs/html/search/all_4.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['gestion_5fpartie_2ec',['gestion_partie.c',['../gestion__partie_8c.html',1,'']]],
-  ['gestion_5fpartie_2eh',['gestion_partie.h',['../gestion__partie_8h.html',1,'']]]
+  ['gestion_5fpartie_2ec',['gestion_partie.c',['../gestion__partie_8c.html',1,'']]]
 ];
--- docs/html/search/all_6.js
+++ docs/html/search/all_6.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['jouer_5fmanche',['jouer_manche',['../gestion__partie_8h.html#a4b7f7e6898db0cd0485400b996767f21',1,'jouer_manche(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU], Joueur *j):&#160;gestion_partie.c'],['../gestion__partie_8c.html#aabc2caddfe36658343dc331c2d919bc2',1,'jouer_manche(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU], Joueur *j):&#160;gestion_partie.c']]],
-  ['jouer_5fpartie',['jouer_partie',['../gestion__partie_8h.html#a125c2e2cc30aedff98dc9676e890d9db',1,'jouer_partie():&#160;gestion_partie.c'],['../gestion__partie_8c.html#a125c2e2cc30aedff98dc9676e890d9db',1,'jouer_partie():&#160;gestion_partie.c']]],
-  ['jouer_5ftour',['jouer_tour',['../gestion__partie_8h.html#a58ea1050fc334f65333957e7b244e389',1,'jouer_tour(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU], Joueur **j):&#160;gestion_partie.c'],['../gestion__partie_8c.html#a58ea1050fc334f65333957e7b244e389',1,'jouer_tour(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU], Joueur **j):&#160;gestion_partie.c']]],
+  ['jouer_5fmanche',['jouer_manche',['../gestion__partie_8c.html#aabc2caddfe36658343dc331c2d919bc2',1,'gestion_partie.c']]],
+  ['jouer_5fpartie',['jouer_partie',['../gestion__partie_8c.html#a125c2e2cc30aedff98dc9676e890d9db',1,'gestion_partie.c']]],
+  ['jouer_5ftour',['jouer_tour',['../gestion__partie_8c.html#a58ea1050fc334f65333957e7b244e389',1,'gestion_partie.c']]],
   ['joueur',['joueur',['../structjoueur.html',1,'']]],
-  ['joueur_5fabandon',['joueur_abandon',['../gestion__partie_8h.html#ab4752abe9709986b3cf2acd2c8307686',1,'joueur_abandon(Joueur *j):&#160;gestion_partie.c'],['../gestion__partie_8c.html#ab4752abe9709986b3cf2acd2c8307686',1,'joueur_abandon(Joueur *j):&#160;gestion_partie.c']]]
+  ['joueur_5fabandon',['joueur_abandon',['../gestion__partie_8c.html#ab4752abe9709986b3cf2acd2c8307686',1,'gestion_partie.c']]]
 ];
