4a1b31d5a3397b26c32932a3844188dd37cafaec
R-Bas <robin.bastide@outlook.fr>
2019-03-05 15:36:00 +0100

    Maj doc le 05/03/19 à 15:35:58

--- docs/html/search/all_9.js
+++ docs/html/search/all_9.js
@@ -3,6 +3,7 @@ var searchData=
   ['piece',['piece',['../structpiece.html',1,'']]],
   ['piece_5fliste_5fcarre',['piece_liste_carre',['../carre_8c.html#ad2737e12ca881a1d070f8a2885d266e1',1,'carre.c']]],
   ['piece_5fliste_5fcreation',['piece_liste_creation',['../carre_8c.html#a205c4dc3c4ea2b46ad2198a9032a0771',1,'carre.c']]],
+  ['piece_5fpivoter',['piece_pivoter',['../carre_8c.html#a3bf1c1c431237cc8353bea53ce34888c',1,'carre.c']]],
   ['piece_5fprecedent',['piece_precedent',['../carre_8c.html#a5a9d4170b6392e768505a510df50fd6b',1,'carre.c']]],
   ['piece_5fsuivant',['piece_suivant',['../carre_8c.html#aab9d9a7003a706e5afa85521cdfb4016',1,'carre.c']]],
   ['prec',['prec',['../structpiece.html#a91811cbe13acb5a84029afd8ab8d9b38',1,'piece::prec()'],['../structjoueur.html#a160f732197c2542736174f6698e4e25e',1,'joueur::prec()']]],
