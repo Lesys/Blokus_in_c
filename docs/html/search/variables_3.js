e79e3001d9725363ce630f09651bbc50c1315eab
R-Bas <robin.bastide@outlook.fr>
2019-03-05 15:24:32 +0100

    Maj doc le 05/03/19 à 15:24:31

--- docs/html/search/variables_3.js
+++ docs/html/search/variables_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['prec',['prec',['../structpiece.html#a91811cbe13acb5a84029afd8ab8d9b38',1,'piece::prec()'],['../structjoueur.html#a160f732197c2542736174f6698e4e25e',1,'joueur::prec()']]]
+  ['prec',['prec',['../structpiece.html#a91811cbe13acb5a84029afd8ab8d9b38',1,'piece::prec()'],['../structjoueur.html#a160f732197c2542736174f6698e4e25e',1,'joueur::prec()']]],
+  ['pseudo',['pseudo',['../structjoueur.html#a99413f433f9baf71bd6b9570bb2f02aa',1,'joueur']]]
 ];
