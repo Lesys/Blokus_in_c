dd8dd7eec66ce2877246db9ae1418237d156ce2a
Widmer Alexis <s165160@ic2s114-20.univ-lemans.fr>
2019-03-26 09:46:49 +0100

    Ecriture des primitives de include/gestion_bot.h

--- include/gestion_bot.h
+++ include/gestion_bot.h
@@ -3,7 +3,15 @@
 
 #include "couleur.h"
 #include "joueur.h"
+#include "carre.h"
 
-int gestion_tour_bot(Couleur pl[][], );
+int gestion_tour_bot(Couleur pl[][], Joueur*);
+
+int bot_jouer(Couleur pl[][], Joueur);
+int adversaire_jouer(Couleur pl[][], Joueur, Joueur); /* Premier Joueur: le bot qui doit jouer. Deuxième Joueur: L'adversaire pour lequel on projette le coup */
+
+int eval_coup_bot(Couleur pl[][], Piece*, Joueur*);
 
 #endif
+
+/* Evaluer la valeur de la case (moins il y a de cases prises autour, plus elle a de valeur) */
