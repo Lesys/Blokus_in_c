dcaaad669119a61626581f5d42106f4d524a00eb
Lesys <alexis.widmer@bts-malraux.net>
2019-03-28 08:52:12 +0100

    Avancée de la fonction d'évaluation pour le bot

--- include/gestion_bot.h
+++ include/gestion_bot.h
@@ -9,20 +9,28 @@ typedef struct coup Coup;
 
 struct coup {
 	Piece* p;
+	Couleur c;
 	int x;
 	int y;
 	int valeur_coup;
 }
 
-int gestion_tour_bot(Couleur pl[][], Joueur*);
+/* Accesseurs */
+Piece* coup_piece (Coup*);
+Couleur coup_couleur (Coup*);
+int coup_coord_x (Coup*);
+int coup_coord_y (Coup*);
+int coup_valeur (Coup*);
+
+int gestion_tour_bot (Couleur pl[][], Joueur*);
 
 
-Coup* bot_jouer_tour(Couleur pl[][], Joueur*);
+Coup* bot_jouer_tour (Couleur pl[][], Joueur*);
 
-int bot_jouer(Couleur pl[][], Joueur, int); /* int: la profondeur de coup (==nb_coup en prévision) */
-int adversaire_jouer(Couleur pl[][], Joueur, Joueur, int); /* Premier Joueur: le bot qui doit jouer. Deuxième Joueur: L'adversaire pour lequel on projette le coup */
+int bot_jouer (Couleur pl[][], Joueur, int); /* int: la profondeur de coup (==nb_coup en prévision) */
+int adversaire_jouer (Couleur pl[][], Joueur, Joueur, int); /* Premier Joueur: le bot qui doit jouer. Deuxième Joueur: L'adversaire pour lequel on projette le coup */
 
-int eval_coup_bot(Couleur pl[][], Piece*, Joueur*);
+int eval_coup_bot (Couleur pl[][], Piece*, Joueur*);
 
 #endif
 
--- src/gestion_bot.c
+++ src/gestion_bot.c
@@ -5,7 +5,33 @@
 
 #include <time.h>
 
-int eval_coup_bot(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU], Piece* p) {} /* TODO */
+static int poser_piece_sdl(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU], Coup* coup) {
+    if(!piece_hors_liste(coup_piece(coup)))
+    {
+        Carre* c = piece_liste_carre(coup_piece(coup));
+        Piece** p = &(j->liste_piece);
+        Piece* pivot = *p;
+
+        do
+        {
+            pl[x+carre_get_x(c)][y+carre_get_y(c)] = coup_couleur(coup);
+            c = carre_get_suiv(c);
+        } while(c != piece_liste_carre(pi));
+/* TODO */
+
+    }
+}
+
+int eval_coup_bot(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU], Coup* coup) {
+	Couleur pl2[TAILLE_PLATEAU][TAILLE_PLATEAU];
+
+	int i, j;
+	for (i = 0; i < TAILLE_PLATEAU; i++)
+		for (j = 0; j < TAILLE_PLATEAU; j++)
+			pl2[i][j] = pl[i][j];
+
+	poser_piece_bot(pl2, coup);
+} /* TODO */
 
 int gestion_tour_bot(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU], Joueur* bot) {
 	Piece* p = bot_jouer_tour(pl, bot);
@@ -33,10 +59,10 @@ Coup* bot_jouer_tour(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU], Joueur* bot)
     srand(time(NULL));
 
     /* Création d'un petit carre*/
-    Piece* carre = malloc(sizeof(Piece));
-	carre->liste_carre = piece_petit_carre();
-	carre->suiv = carre;
-	carre->prec = carre;
+/*    Piece* carre = malloc(sizeof(Piece));
+    carre->liste_carre = piece_petit_carre();
+    carre->suiv = carre;
+    carre->prec = carre;*/
 
     Piece * p = joueur_liste_piece(bot);
     Piece * init = p;
@@ -70,12 +96,19 @@ Coup* bot_jouer_tour(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU], Joueur* bot)
 
                         tab[compteur]->p = piece_copie(p);
 
-                        /* tab[compteur]->valeur_coup = eval_coup_bot(pl, bot, tab[compteur]->p); */
+			/* Enlève la Piece actuelle de la liste temporairement */
+			tab[compteur]->p->prec->suiv = tab[compteur]->p->suiv;
 
+			/* Affecte le Coup dans le tableau */
                         tab[compteur]->x = i;
                         tab[compteur]->y = j;
 
-                        adversaire_jouer();
+                        /* tab[compteur]->valeur_coup = eval_coup_bot(pl, bot, tab[compteur]->p); */
+
+                        adversaire_jouer(pl, joueur_suivant(bot));
+
+			/* Remet la Piece dans la liste */
+			tab[compteur]->p->prec->suiv = tab[compteur];
                     }
 
                     changer_orientation(p)
