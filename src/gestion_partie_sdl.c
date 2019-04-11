f0ae01a74b6dff1dd922e55f035346aef0ad5af6
Snalts <alexandre.jodeau@orange.fr>
2019-04-11 16:45:50 +0200

    Ajout de la valeur de la partie dans jouer_manche_sdl pour evité d'initialiser le plateau*/

--- src/gestion_partie_sdl.c
+++ src/gestion_partie_sdl.c
@@ -793,7 +793,7 @@ int initialisation_charger_partie(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU],Jou
 				*j=joueur_suivant(*j);
 			} while( *j != pivot);
 		}
-	continuer= 1;
+	continuer= 0;
 	}
 	else{
 		return 2;
@@ -870,12 +870,15 @@ int attente_nouvelle_partie(Joueur * j) {
 */
 
 
-int jouer_manche_sdl(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU],Joueur* j){
+int jouer_manche_sdl(Couleur pl[TAILLE_PLATEAU][TAILLE_PLATEAU],Joueur* j, int valeur_partie){
 	int choix;
         Joueur * init;
 	do{
+		if(valeur_partie == 5)
+			val_partie = 1;
+		else{
 		initialisation_manche(pl,&j);
-
+		}
 		do{
 
 	            init = j;
@@ -1121,7 +1124,7 @@ int jouer_partie_sdl(){ /*Appel de toute les fonctions partie */
 
 			else if (retour != 3){ /* Si les Joueurs arrêtent le programme pendant la saisie des pseudos / nb_joueur */
 				if(val_partie == 1 || val_partie == 5)
-					retour = jouer_manche_sdl(pl,j);
+					retour = jouer_manche_sdl(pl,j,val_partie);
 				else if(val_partie == 2)
 					retour = jouer_manche_distant_sdl(pl, j, retour);
 			/*?*/	joueur_liste_detruire(&j);
